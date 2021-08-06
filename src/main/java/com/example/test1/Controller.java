24d2de2aecadb692ff2ba5f36327c699f515e191
Ligit2 <avoyan-lilit@mail.ru>
2021-08-06 16:53:34 +0400

    commit

--- src/main/java/com/example/test1/Controller.java
+++ src/main/java/com/example/test1/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+
     @PostMapping("/lilith")
     public String getLilith() {
         return "Lilith";
